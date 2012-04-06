0d30557f6f345d4f869d976156fd43e2daa63115
Martin T. H. Sandsmark <sandsmark@samfundet.no>
2012-04-06 18:07:19 +0200

    release

--- graphics.cpp
+++ graphics.cpp
@@ -113,7 +113,6 @@ void Graphics::display(void) {
     suseconds_t diff = cur - m_lastUpdate;
     m_lastUpdate = cur;
 
-        outroDisplay(cur, diff);    glutSwapBuffers();   return;
     if (cur < 10000000) {
         introDisplay(cur, diff);
     } else if (cur < 50000000) {
