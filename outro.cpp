df60a97ceb74a4bf0f3c50c97f69592ebccc2664
Martin T. H. Sandsmark <sandsmark@samfundet.no>
2012-04-06 17:52:38 +0200

    fix shit

--- outro.cpp
+++ outro.cpp
@@ -5,9 +5,26 @@ void Graphics::outroDisplay(suseconds_t cur, suseconds_t diff) {
                0, 0, 0,  // Focus
                0.0,  1.0,  0.0); // Up
 
+    int bass = m_sound.getBass();
+    m_angleSpeed += bass;
+    m_angleSpeed /= 1.05;
+    if (m_angleSpeed > 1) m_angleSpeed = 1;
+    m_angle += diff*(m_angleSpeed+1) / 5000;
+    if (m_angle > 360) m_angle = -360;
+    glRotatef(m_angle/2, 1, 1, 1);
+
+    if (bass > 85) {
+        m_scale += bass;
+    }
+
+    m_scale /= 1.1;
+    if (m_scale > 1) m_scale = 1;
+    //glScalef(m_scale+1, m_scale+1, m_scale+1);
+
+
 //    50000000
 //    if (m_angleSpeed > 1) m_angleSpeed = 1;
-    m_angle += diff / 50000;
+/*    m_angle += diff / 50000;
     printf("%d\n", m_angle);
     if (m_angle > 360) m_angle = -360;
 
@@ -15,12 +32,12 @@ void Graphics::outroDisplay(suseconds_t cur, suseconds_t diff) {
         if (m_angle > 90) m_angle = 90;
         glRotatef(m_angle/2, 0, 0, 1);
     } else if (cur < 54000000) {
-        if (m_angle > 90) m_angle = 90;
+        if (m_angle > 270) m_angle = 270;
         glRotatef(m_angle/2, 0, 1, 0);
     } else if (cur < 56000000) {
-        if (m_angle > 90) m_angle = 90;
+        if (m_angle > 180) m_angle = 180;
         glRotatef(m_angle/2, 1, 0, 0);
-    }
+    }*/
     for (int i=outroOffset+4; i<m_callLists.size(); i++)
        glCallList(m_callLists[i]);
 
