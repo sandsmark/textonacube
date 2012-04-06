ce7e1028e112ac1624d3da3facc883c261e12533
Amund Hov <amundhov@samfundet.no>
2012-04-06 17:51:16 +0200

    intro

--- intro.cpp
+++ intro.cpp
@@ -1,11 +1,28 @@
 #include "graphics.h"
+#include "math.h"
 #include <GL/gl.h>
 
+#define PI 3.14159265
+
 void Graphics::introDisplay(suseconds_t cur, suseconds_t diff) {
-	glRotatef(90,0,1,0);
-    gluLookAt(-2,  -6, 0, // Eye
+	bool toggled = false;
+	float angle = 90;
+
+    int bass = m_sound.getBass();
+
+	// glRotatef(90,0,0,1); // Legendary
+	glRotatef(90,0,0,1);
+    gluLookAt(-2 + (float)bass / 100.0f,  -6, 0, // Eye
                0, 0, 0,  // Focus
                0.0,  1.0,  0.0); // Up
+
+
+	if ( cur > 2359376  && cur < 4816286) {
+		glRotatef(90,1,0,0);
+		glRotatef(-90,0,0,1);
+	} else if ( cur > 4816286 ) {
+		glRotatef(-90,0,0,1);
+	}
 
     for (int i=0; i<sceneOffset; i++)
        glCallList(m_callLists[i]);
